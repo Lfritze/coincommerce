deda9cb011b1bbdcc594b7399f4e2a48d1432e26
Leighton Fritze <Jay.Row.Fritze@gmail.com>
2020-10-10 12:00:38 -0400

    imported mongoose and connected database in server.js from .env

--- server.js
+++ server.js
@@ -1,8 +1,19 @@
 const express = require('express');
+const mongoose = require('mongoose');
 require('dotenv').config();
+
+// Server
 const server = express();
 
+// Database
 
+mongoose.connect(process.env.DATABASE, {
+  useNewUrlParser: true,
+  useCreateIndex: true
+})
+.then(() => console.log('Database Connected '))
+
+// Routes
 server.get('/', (req, res) => {
   res.send('hello from node')
 });
