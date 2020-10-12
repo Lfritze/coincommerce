4fbd18d1a5ce9b6a1bd3ddf3af6c019c838da304
Leighton Fritze <Jay.Row.Fritze@gmail.com>
2020-10-11 22:14:58 -0400

    setup user controller and post in user route

--- routes/user.js
+++ routes/user.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const router = express.Router();
 // import from controllers
-const { sayHi } = require('../controllers/user');
+const { signUp } = require('../controllers/user');
 
-router.get('/', sayHi);
+// any time there is a request coming in the 'signUp' this controller method will run - see /controllers/user.js
+router.post('/signup', signUp);
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,5 +1,11 @@
 const express = require('express');
 const mongoose = require('mongoose');
+// MORGAN is a middleware that allows us to easily log requests, errors, and more to the console
+const morgan = require('morgan');
+// body-parser extracts the entire body portion of an incoming request stream and exposes it on req.body
+const bodyParser = require('body-parser')
+// We will be saving the user credentials in the cookie - A cookie is a small piece of data that a server sends to the user's web browser
+const cookieParser = require('cookie-parser')
 require('dotenv').config();
 // import routes
 const userRoutes = require('./routes/user')
@@ -13,6 +19,13 @@ mongoose.connect(process.env.DATABASE, {
   useCreateIndex: true
 })
 .then(() => console.log('Database Connected '))
+
+// Middleware
+// use a dev flag with morgan
+server.use(morgan('dev'));
+// so that we get JSON data from the req.body
+server.use(bodyParser.json());
+server.use(cookieParser());
 
 // Routes Middleware **************
 // prepend api so any user route starts with api
