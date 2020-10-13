371a9f714caeb1db1be8a8adebeca54bdcd684d0
Leighton Fritze <Jay.Row.Fritze@gmail.com>
2020-10-13 11:02:31 -0400

    added dbErrorHandler.js for error messaging and made salt & hashed_password undefined when creating a new user - This is so that we do not expose these things

--- controllers/user.js
+++ controllers/user.js
@@ -1,5 +1,7 @@
 // import our 'User' from the userSchema where it is exported at the bottom
-const User = require('../models/user')
+const User = require('../models/user');
+// import our error message helper
+const { errorHandler } = require("../helpers/dbErrorHandler");
 
 exports.signUp = (req, res) => {
   // we are going to create a new user based on what we get from the req.body
@@ -10,9 +12,14 @@ exports.signUp = (req, res) => {
   user.save((err, user) => {
     if(err) {
       return res.status(400).json({
-        err
+        // use our error message helper and pass in 'err' as an argument
+        err: errorHandler(err)
       })
     }
+    // when we create a new user we do not want to expose their 'salt' and 'hashed_password'
+    // so we set it to undefined
+    user.salt = undefined;
+    user.hashed_password = undefined;
     res.json({
       user
     })
