733127c63282c540f640978e3b3d9083a58c26d9
Leighton Fritze <Jay.Row.Fritze@gmail.com>
2020-10-23 16:05:21 -0400

    implemented remove middleware in product controller to delete products

--- routes/product.js
+++ routes/product.js
@@ -1,13 +1,12 @@
 const express = require('express');
 const router = express.Router();
 // import from controllers
-const { create, productById, read } = require('../controllers/product');
+const { create, productById, read, remove } = require('../controllers/product');
 const { requireSignin, isAuth, isAdmin } = require('../controllers/auth');
 const { userById } = require('../controllers/user');
 
 // we want to get the single product and READ the product (this is based on CREAT, READ, UPDATE, DELETE)
 router.get('/product/:productId', read)
-
 
 // any time there is a request coming in the 'signUp' this controller method will run - see /controllers/user.js
 // same thing with the userSignupValidator
@@ -17,6 +16,14 @@ router.post("/product/create/:userId",
   isAdmin, 
   create
 );
+
+// Delete a Product
+router.delete('/product/:productId/:userId', 
+  requireSignin, 
+  isAuth,
+  isAdmin,
+  remove 
+  )
 
 // we are just taking the parameter 'userId' rather than using a 'get' method
 // anytime there is userId in the route - we run the middleware 'userById' and makes the user available in the request
