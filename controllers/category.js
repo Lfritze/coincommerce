6987ec7bd0e988c9ba65f97b5927362d13d60a42
Leighton Fritze <Jay.Row.Fritze@gmail.com>
2020-10-23 20:32:28 -0400

    implemented category READ method

--- controllers/category.js
+++ controllers/category.js
@@ -1,5 +1,22 @@
 const Category = require('../models/category');
 const { errorHandler } = require("../helpers/dbErrorHandler");
+
+exports.categoryById = (req, res, next, id) => {
+  // first we need to find the category
+  // we get the 'id' from the route parameter
+  // then we execute a method that gives us either error or the category
+  Category.findById(id).exec((err, category) => {
+    if (err || !category) {
+      return res.status(400).json({
+        error: "Category does not exist"
+      });
+    }
+    // if we find the category we can add it to the request object
+    req .category = category;
+    next();
+  });
+};
+
 exports.create = (req, res) => {
   // create a new category
   const category = new Category(req.body)
@@ -12,4 +29,9 @@ exports.create = (req, res) => {
     }
     res.json({ data });
   });
+};
+
+// we are just returning the category
+exports.read = (req, res) => {
+    return res.json(req.category);
 };
