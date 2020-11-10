b58a7c5b53ec52fc16fb3dc522061936dcece44c
Leighton Fritze <Jay.Row.Fritze@gmail.com>
2020-11-10 16:00:58 -0500

    added layout to signin, signup andd moved Menu from Routes.js to Layout.js

--- frontend/src/core/Layout.js
+++ frontend/src/core/Layout.js
@@ -1,12 +1,14 @@
 import React from 'react';
+import Menu from './Menu'
 
 // props title, description, children
-const Layout = ({title = 'Title', 
-  description="Just a filler", 
+const Layout = ({title = 'Title Filler', 
+  description="Description Filler", 
   className, 
   children
 }) => (
   <div>
+    <Menu />
     <div className="jumbotron">
     <h2>{title}</h2>
     <p className="lead">{description}</p>
--- frontend/src/user/Signup.js
+++ frontend/src/user/Signup.js
@@ -1,5 +1,9 @@
 import React from 'react';
+import Layout from '../core/Layout';
 
-const Signup = () => <div>Signup</div>
+const Signup = () => (
+<Layout title="Signup" description="Signup for Node React E-commerce App">
+  ...
+</Layout>)
 
 export default Signup;
