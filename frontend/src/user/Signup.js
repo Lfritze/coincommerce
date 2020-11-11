e3c5c42c8859c5bdc453c08445e95ac667b3b500
Leighton Fritze <Jay.Row.Fritze@gmail.com>
2020-11-10 23:00:04 -0500

    fetch POST for user signup JSON stringify JabaScript body to backend

--- frontend/src/user/Signup.js
+++ frontend/src/user/Signup.js
@@ -17,14 +17,48 @@ const Signup = () => {
     error: '',
     success: false
   })
+  // Let's de-structure the values above
+  const {name, email, password} = values
+  // now we can create a new user with these values
+
   // Now we need a handleChange method
   // this is going to be a higher order function (a function returning another function)
-
   // we grab the event.target.value
   const handleChange = name => event => {
     // take the rest of the values with a spread operator, set error to false
     // then we take the name (it will be dynamic, meaning it could be name, email or password) and set it to event.target.value
     setValues({...values, error: false, [name]: event.target.value });
+  }
+
+  // to create a new user we create a new method 
+  // user comes from the clickSubmit below
+  // the Javascript object { name, email, password } represents the USER 
+  const signup = (user) => {
+    // console.log(name, email, password)
+    // Now we need to send this data to the backend - we can use FETCH
+    fetch(`${API}/signup`, {
+      method: "POST",
+      headers: {
+        Accept: 'application/json',
+        "Content-Type": "application/json"
+      },
+      // We can't send the JavaScript object to the backend so we use JSON Stringify 
+      body: JSON.stringify(user)
+    })
+    .then(response => {
+      return response.json()
+    })
+    .catch(err => {
+      console.log(err)
+    })
+  }
+
+  const clickSubmit = (event) => {
+    // prevent the default behaviour of the browser - when the button is clicked we don't want the page to reload
+    event.preventDefault()
+    // grab the name, email, password (javaScript object) from state because we need to send the data to the backend
+    signup({ name, email, password });
+
   }
 
   // Function to show the signup FORM
@@ -42,7 +76,7 @@ const Signup = () => {
         <label className="text-muted">Password</label>
         <input onChange={handleChange('password')} type="password" className="form-control" />
       </div>
-      <button className="btn btn-primary">Submit</button>
+      <button onClick={clickSubmit} className="btn btn-primary">Submit</button>
     </form>
   );
   return (
@@ -53,7 +87,7 @@ const Signup = () => {
       className="container col-md-8 offset-md-2">
       {signUpForm()} 
       {/* JSON stringify so when there is a change - we can see the data live (whatever data we have in the state we will be able to see) */}
-      {JSON.stringify(values)}
+      {/* {JSON.stringify(values)} */}
     </Layout>
   );
 };
