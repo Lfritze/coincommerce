1cb07e08a97bc826cc6825f0a915f5b37d653bcc
Leighton Fritze <Jay.Row.Fritze@gmail.com>
2020-11-10 19:06:02 -0500

    setup signup with signUpForm()

--- frontend/src/user/Signup.js
+++ frontend/src/user/Signup.js
@@ -1,9 +1,31 @@
 import React from 'react';
 import Layout from '../core/Layout';
+// import { API } from '../config';
 
-const Signup = () => (
-<Layout title="Signup" description="Signup for Node React E-commerce App">
-  ...
-</Layout>)
+const Signup = () => {
+  // Function to show the signup FORM
+  const signUpForm = () => {
+    <form>
+      <div className="form-group">
+        <label className="text-muted">Name</label>
+        <input type="text" className="form-control" />
+      </div>
+      <div className="form-group">
+        <label className="text-muted">Email</label>
+        <input type="email" className="form-control" />
+      </div>
+      <div className="form-group">
+        <label className="text-muted">Password</label>
+        <input type="password" className="form-control" />
+      </div>
+    </form>
+  }
+  return (
+    <Layout title="Signup" description="Signup for Node React E-commerce App">
+      {signUpForm()}
+    </Layout>
+  );
+};
+
 
 export default Signup;
