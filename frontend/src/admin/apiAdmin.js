a397cb79295053fc202803264bb7038ada7a16d9
Leighton Fritze <Jay.Row.Fritze@gmail.com>
2020-11-12 16:25:43 -0500

    setup AddProduct and createProduct API request - currently getting 400 bad request for POST request

--- frontend/src/admin/apiAdmin.js
+++ frontend/src/admin/apiAdmin.js
@@ -21,3 +21,34 @@ export const createCategory = (userId, token, category) => {
       console.log(err)
     });
   };
+
+  export const createProduct = (userId, token, product) => {
+    // console.log(name, email, password)
+    // Now we need to send this data to the backend - we can use FETCH
+    return fetch(`${API}/product/create/${userId}`, {
+      method: "POST",
+      headers: {
+        Accept: 'application/json',
+        // we are sending FORM DATA
+        Authorization: `Bearer ${token}`
+      },
+      // 
+      body: product
+    })
+    .then(response => {
+      return response.json()
+    })
+    .catch(err => {
+      console.log(err)
+    });
+  };
+
+  export const getCategories = () => {
+    return fetch(`${API}/categories`, {
+        method: 'GET'
+    })
+        .then(response => {
+            return response.json();
+        })
+        .catch(err => console.log(err));
+};
