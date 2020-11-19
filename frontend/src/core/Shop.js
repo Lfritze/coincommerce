de417ba28626702636aba076f58ca2d88095330d
Leighton Fritze <Jay.Row.Fritze@gmail.com>
2020-11-18 22:32:17 -0500

    created load more button with funtionality in shop.js

--- frontend/src/core/Shop.js
+++ frontend/src/core/Shop.js
@@ -10,11 +10,12 @@ const Shop = () => {
   const [myFilters, setMyFilters] = useState({
     filters:  { category: [], price: [] } 
   })
-  const [categories, setCategories] = useState([])
-  const [error, setError] = useState(false)
-  const [limit, setLimit] = useState(6)
-  const [skip, setSkip] = useState(0)
-  const [filteredResults, setFilteredResults] = useState([])
+  const [categories, setCategories] = useState([]);
+  const [error, setError] = useState(false);
+  const [limit, setLimit] = useState(6);
+  const [skip, setSkip] = useState(0);
+  const [size, setSize] = useState(0);
+  const [filteredResults, setFilteredResults] = useState([]);
 
   const init = () => {
     // loads categories
@@ -31,11 +32,35 @@ const Shop = () => {
     // console.log(newFilters)
     getFilteredProducts(skip, limit, newFilters).then(data => {
       if(data.error) {
-        setError(data.error)
+        setError(data.error);
       } else {
-        setFilteredResults(data.data)
+        setFilteredResults(data.data);
+        setSize(data.size);
+        setSkip(0);
       }
-    })
+    });
+  };
+
+  const loadMore = () => {
+    // skip by default is 0 and limit is 6
+    let toSkip = skip + limit
+    getFilteredProducts(toSkip, limit, myFilters.filters).then(data => {
+      if(data.error) {
+        setError(data.error);
+      } else {
+        setFilteredResults([...filteredResults, ...data.data]);
+        setSize(data.size);
+        setSkip(toSkip);
+      }
+    });
+  };
+
+  const loadMoreButton = () => {
+    return (
+      size > 0 && size >= limit && (
+        <button onClick={loadMore} className="btn-warning mb-5">Load More</button>
+      )
+    )
   }
 
   useEffect(() => {
@@ -92,7 +117,8 @@ const Shop = () => {
                   <Card key={i} product={product} />
               ))}
             </div>
-
+            <hr />
+            {loadMoreButton()}
           </div>
       </div>
     </Layout>
