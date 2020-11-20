d36c2c98272efaf4afece50bef6c2f3c7f5d00b5
Leighton Fritze <Jay.Row.Fritze@gmail.com>
2020-11-19 20:42:29 -0500

    created search bar and option category select to display on home page

--- frontend/src/core/Search.js
+++ frontend/src/core/Search.js
@@ -27,10 +27,44 @@ const Search = () => {
 
   useEffect(() => {
     loadCategories()
-  }, [])
+  }, []);
+
+  const searchSubmit = () => {
+    //
+  }
+
+  const handleChange = () => {
+    // dropdown of categories
+  }
+
+  const searchForm = () => {
+    return (
+      <form onSubmit={searchSubmit}>
+        <span className="input-group-text">
+          <div className="input-group input-group-lg">
+            <div className="input-group-prepend">
+              <select className="btn mr-2" onChange={handleChange("category")}>
+                <option value="All">Pick Category</option>
+                {categories.map((cat, i) => (<option key={i} value={cat._id}>{cat.name}
+                </option>))}
+              </select>
+            </div>
+            <input type="search" className="form-control" onChange={handleChange('search')} placeholder="Search by name" />
+          </div>
+          <div className="btn input-group-append" style={{border: 'none'}}>
+            <button className="input-group-text">Search</button>
+          </div>
+        </span>
+      </form>
+    )
+  }
 
   return (
-    <div><h2>Search Bar {JSON.stringify(categories)}</h2></div>
+    <div className="row">
+      <div className="container">
+        {searchForm()}
+      </div>
+    </div>
   )
 }
 
