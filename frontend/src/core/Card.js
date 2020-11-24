bc0ff5108865c1223e8a355170d3f84daedb6543
Leighton Fritze <Jay.Row.Fritze@gmail.com>
2020-11-23 21:52:31 -0500

    added product detail on single page products and implemented styling

--- frontend/src/core/Card.js
+++ frontend/src/core/Card.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import ShowImage from './ShowImage';
+import moment from 'moment';
 
 const Card = ({product, showViewProductButton = true}) => {
 
@@ -14,23 +15,40 @@ const Card = ({product, showViewProductButton = true}) => {
     );
   };
 
+  const showAddToCartButton = () => {
+    return (
+      <button className="btn btn-outline-success mt-2 mb-2">
+              Add to cart
+            </button>
+    );
+  }
+
+  const showStock = (quantity) => {
+    return quantity > 0 ? (
+      <span className="badge badge-primary badge-pill">In Stock</span> 
+    ) : (
+      <span className="badge badge-primary badge-pill">Out of Stock</span> 
+    );
+  };
+
   return (
     
       <div className="card">
-        <div className="card-header">
-          {product.name}
-          <div className="card-body">
-            <ShowImage item={product} url="product" />
-            {/* // This is so we only show the first 100 characters in the description */}
-            <p>{product.description.substring(0, 100)}</p>
-            <p>${product.price}</p>
-            {showViewButton(showViewProductButton)}          
-            <button className="btn btn-outline-success mt-2 mb-2">
-              Add to cart
-            </button>
+        <div className="card-header name">{product.name}</div>
+        <div className="card-body">
+          <ShowImage item={product} url="product" />
+          {/* // This is so we only show the first 100 characters in the description */}
+          <p className="card-p mt-2">{product.description.substring(0, 100)}</p>
+          <p className="card-p black-10">${product.price}</p>
+          <p className="black-9">Category: {product.category && product.category.name}</p>
+          <p className="black-8">Added: {moment(product.createdAt).fromNow()}</p>
+          {showStock(product.quantity)}
+          <br />
+          {showViewButton(showViewProductButton)}          
+          {showAddToCartButton()}
           </div>
         </div>
-      </div>
+      
     
   )
 }
