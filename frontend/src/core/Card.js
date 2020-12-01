bfafd1d60e120929b25f304e12402d0d9a67a635
Leighton Fritze <Jay.Row.Fritze@gmail.com>
2020-12-01 13:57:36 -0500

    created remove product from cart feature

--- frontend/src/core/Card.js
+++ frontend/src/core/Card.js
@@ -2,13 +2,14 @@ import React, { useState } from 'react';
 import { Link, Redirect } from 'react-router-dom';
 import ShowImage from './ShowImage';
 import moment from 'moment';
-import {addItem, updateItem} from './cartHelpers'
+import {addItem, updateItem, removeItem } from './cartHelpers'
 
 const Card = ({ 
   product, 
   showViewProductButton = true, 
   showAddToCartButton = true,
-  cartUpdate = false
+  cartUpdate = false,
+  showRemoveProductButton = false
 }) => {
 
   const [rediredt, setRedirect] = useState(false)
@@ -40,8 +41,18 @@ const Card = ({
 
   const showAddToCart = (showAddToCartButton) => {
     return showAddToCartButton && (
-      <button onClick={addToCart} className="btn btn-outline-success mt-2 mb-2">
-              Add to cart
+      <Link to="/cart"> 
+        <button onClick={addToCart} className="btn btn-outline-success mt-2 mb-2">
+          Add to cart
+        </button>
+      </Link>
+    );
+  };
+
+  const showRemoveButton = (showRemoveProductButton) => {
+    return showRemoveProductButton && (
+      <button onClick={() => removeItem(product._id) } className="btn btn-outline-danger mt-2 mb-2">
+              Remove Product
             </button>
     );
   }
@@ -96,7 +107,9 @@ const Card = ({
           {/* these are props */}
           {showViewButton(showViewProductButton)}
           {showAddToCart(showAddToCartButton)}
+          {showRemoveButton(showRemoveProductButton)}
           {showCartUpdateOptions(cartUpdate)}
+          
           </div>
         </div>
       
