a02e2a0e8e558f16d148b7bd33c1d5bf599bc230
Leighton Fritze <Jay.Row.Fritze@gmail.com>
2020-12-06 23:12:47 -0500

    implemented the ability to save delivery address to the database

--- frontend/src/core/Checkout.js
+++ frontend/src/core/Checkout.js
@@ -61,6 +61,8 @@ const Checkout = ({products, setRun = f => f, run = undefined}) => {
     )
   };
 
+  let deliveryAddress = data.address
+
   const buy = () => {
     // to diplay loading message
     setData({ loading: true})
@@ -91,7 +93,7 @@ const Checkout = ({products, setRun = f => f, run = undefined}) => {
           products: products,
           transaction_id: response.transaction_id,
           amount: response.transaction.amount,
-          address: data.address
+          address: deliveryAddress
         }
         createOrder(userId, token, createOrderData)
         // empty cart
